9f42daf8da18857acca74164b287bacc49fffc65
Muhammad Rivki <Muhammad.Rivki@live.com>
2020-05-31 17:43:42 +0700

    :tada: initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+
+const app = express()
+const PORT = 3001
+
+mongoose.connect("mongodb://localhost:27017/node-jwt", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "welcome",
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`App run on http://localhost:${PORT}`)
+})
