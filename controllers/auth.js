c3fc90766a7d7754b96f5580bd9e1c1b8fd81b24
Muhammad Rivki <Muhammad.Rivki@live.com>
2020-06-01 14:44:06 +0700

    :sparkles: add handler login

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,8 +25,20 @@ module.exports.handleSignup = (req, res) => {
 }
 
 module.exports.handleLogin = (req, res) => {
-  res.json({
-    message: 'Login',
+  const { email, password } = req.body
+
+  User.findOne({ email }, (err, user) => {
+    if (err) return res.status(500).json({ message: err })
+    if (!user) return res.status(404).json({ message: 'User not found' })
+
+    const isPasswordValid = bcrypt.compareSync(password, user.password)
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Password invalid' })
+    }
+
+    res.status(200).json({
+      data: user,
+    })
   })
 }
 
